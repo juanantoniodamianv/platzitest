65ead23ab882e76c665353743e1a314028d7ec8c
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2020-08-18 21:26:25 -0300

    Common matchers.

---
+++ __tests__/matchers.test.js
@@ -0,0 +1,14 @@
+describe('Comparadores comunes', () => {
+  const user = {
+    name: "Oscar",
+    lastName: "Barajas"
+  }
+  const user2 = {
+    name: "Oscar",
+    lastName: "Barajas"
+  }
+
+  test('Igualdad de elementos', () => {
+    expect(user).toEqual(user2);
+  });
+});
